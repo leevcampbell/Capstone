e4e6a6db3e17224a44c75a19167f7d05463839ca
LeeVincentCampbell <leevcampbell@gmail.com>
2023-02-14 19:54:55 -0500

    changed message in script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "this content added by JavaScript";
+h2.textContent = "Hella dope dopeness will be here in time pleyboi!";
 document.querySelector("body").appendChild(h2);
